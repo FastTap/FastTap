6fb1ca5caf9e7ef6539a3703c572b4acedb195a6
Stuart Adamson <Stuz719@users.noreply.github.com>
2016-07-29 06:23:29 +0100

    Update Fasttap_object.js
    Amended so that only the ID is set, but comments indicate other properties which could be associated with the object

--- Fasttap_object.js
+++ Fasttap_object.js
@@ -1,23 +1,29 @@
-function taskTaskObject() {
+// JavaScript File
+function taskTaskObject(taskUniqueID) {
 //code written by Stuart Adamson
 //Copyright 2016
 //Licensed as per Apache 2.0 license
-taskID:"", //unique identifier
-taskName:"", //name of task
-taskCreator:"", //who created the task
-taskOwner:"", //who is assigned the task - can be a group
-taskCreatedDate:"", //date the task was created
-taskCreatedTime:"", //time that the task was created
-taskDueDate:[], //date that the task is due to be completed
-taskDueTime:"", //time task is due to be completed
-taskDueDateAmended:[], //array of dates when the due date was amended (with time)
-taskDueDateHistory:[], //array of history of due dates and times
-taskDueDateAmendedBy:[], //who amended the due date and time
-taskDescription:"", //free text description of the task
-taskVisibility:"", //visible to owner only, owner plus individual (or group), or global
-taskVisibilityGroups:[], //array of visibility settings where available to multiple individuals or groups
-taskPriority:"", // task priority
-taskCompletion:[],//percentage completed
-taskCompletionAmended:[]//timestamp array of changes to completion percentage
+//
+taskID:taskUniqueID; //unique identifier
+//
+//all the following are optional properties which can be set later
+//
+//taskName:""; //name of task
+//taskCreator:""; //who created the task
+//taskOwner:""; //who is assigned the task - can be a group
+//taskCreatedDate:""; //date the task was created
+//taskCreatedTime:""; //time that the task was created
+//taskDueDate:[]; //date that the task is due to be completed
+//taskDueTime:""; //time task is due to be completed
+//taskDueDateAmended:[]; //array of dates when the due date was amended (with time)
+//taskDueDateHistory:[]; //array of history of due dates and times
+//taskDueDateAmendedBy:[]; //who amended the due date and time
+//taskDescription:""; //free text description of the task
+//taskVisibility:""; //visible to owner only, owner plus individual (or group), or global
+//taskVisibilityGroups:[]; //array of visibility settings where available to multiple individuals or groups
+//taskPriority:""; // task priority
+//taskCompletion:[];//percentage completed
+//taskCompletionAmended:[]//timestamp array of changes to completion percentage
+//
 //could add in other properties to maintain a record of changes to objects 
 }
